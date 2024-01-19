234cdac7ae19bb677c6fa21347d1a89cd82f32f6
robertsoram <robertsoram@gmail.com>
2024-01-19 10:16:23 +0530

    adding Autoloader function

--- includes/autoloader.inc.php
+++ includes/autoloader.inc.php
@@ -1,3 +1,13 @@
 <?php
-    spl_autoload_register()
+
+spl_autoload_register($fullPath);
     
+function myAutoLoader($classname) {
+    $path = "classes/";
+    $extension = ".class.php";
+    $fullPath = $path . $classname . $extension;
+
+    include_once $fullPath;
+}
+
+?>
